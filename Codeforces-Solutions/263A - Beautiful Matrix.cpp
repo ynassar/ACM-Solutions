21438d1004f304214578dca7d85fea0e0152cd0b
ynassar <yousefnassar@aucegypt.edu>
2016-04-09 10:08:40 +0200

    Added Explanation

--- Codeforces-Solutions/263A - Beautiful Matrix.cpp
+++ Codeforces-Solutions/263A - Beautiful Matrix.cpp
@@ -1,3 +1,12 @@
+/*
+	Solution Explanation:
+		The number of swaps required to move the 1 from any row a to any other row b is the absolute value of (a - b).
+		The number of swaps required to move the 1 from any column c to any other column d is the absolute value of (c - d).
+		Thus, the total number of swaps required to move the 1 from row i and column j to row x and column y is
+		|i-x| + |j-y|. The problem asks for the miniumum number of swaps required to move the 1 from it's current position
+		to the center of the matrix (which is at row 2 and column 2). To do this we first identify the row i where the 1 is,
+		and the column j where the 1 is. Our answer is then |i - 2| + |j - 2|.
+*/
 #include <iostream>
 using namespace std;
 
