80ff02fab50be60fba8b4a004f9f94604ad14655
ynassar <yousefnassar@aucegypt.edu>
2016-04-09 10:00:03 +0200

    Added Explanation

--- Codeforces-Solutions/109A - Lucky Sum of Digits.cpp
+++ Codeforces-Solutions/109A - Lucky Sum of Digits.cpp
@@ -1,3 +1,11 @@
+/*
+	Solution Explanation: 
+	To create the lucky number with the required sum of digits x, you need n 4's and m 7's such that
+	n*4 + m*7 = x. Looping over all possible values of m, you can obtain values for n = (x - m*7)/4 
+	only if (x - m * 7) is divisible by 4. Out of all values of n and m, the minimum lucky number will have the
+	smallest total number of digits, i.e. the smallest n + m. The lucky number is impossible to construct if for all values
+	of m, there is no suitable value of n.
+*/
 #include <iostream>
 #include <string>
 using namespace std;
