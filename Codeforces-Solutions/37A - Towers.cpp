4076a733f3977e04f4fc33a825e4764f58316d24
ynassar <yousefnassar@aucegypt.edu>
2016-04-10 18:29:38 +0200

    Update 37A - Towers.cpp

--- Codeforces-Solutions/37A - Towers.cpp
+++ Codeforces-Solutions/37A - Towers.cpp
@@ -1,3 +1,9 @@
+/*
+	Solution Explanation:
+	Combine all bars of equal height. Keep track of the heights that have already been combined, either by deleting them 
+	from the vector/array, or by keeping another array that records whether a given height has been used. Combining n bars
+	reduces the number of towers by n-1.
+*/
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -13,23 +19,23 @@ int NumberOfBarsWithHeight(int height, const vector<int>& barHeights)
 }
 int main()
 {
-    int numberOfBars;
+	int numberOfBars;
 	cin >> numberOfBars;
-    vector<int> barHeights;
+	vector<int> barHeights;
 	barHeights.resize(numberOfBars);
 	vector<bool> heightUsed(1001, false);
-    for(int i = 0; i < numberOfBars; i++)
-        cin >> barHeights[i];
+	for(int i = 0; i < numberOfBars; i++)
+	cin >> barHeights[i];
 	int numberOfTowers = numberOfBars, maximumHeight = 1;
-    for(int i = 0; i < numberOfBars; i++)
-    {
+	for(int i = 0; i < numberOfBars; i++)
+	{
 		int numberOfBarsWithHeight = NumberOfBarsWithHeight(barHeights[i], barHeights);
 		if (numberOfBarsWithHeight >= 2 && !heightUsed[barHeights[i]])
-        {
+		{
 			numberOfTowers -= (numberOfBarsWithHeight - 1);
 			heightUsed[barHeights[i]] = true;
 			maximumHeight = max(maximumHeight, numberOfBarsWithHeight);
-        }
-    }
+		}
+	}
 	cout << maximumHeight << " " << numberOfTowers;
 }
