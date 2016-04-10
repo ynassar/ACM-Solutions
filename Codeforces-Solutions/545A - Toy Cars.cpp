2584527ba150962eba394a67fcb66cf0cf049f51
ynassar <yousefnassar@aucegypt.edu>
2016-04-10 18:33:35 +0200

    Update 545A - Toy Cars.cpp

--- Codeforces-Solutions/545A - Toy Cars.cpp
+++ Codeforces-Solutions/545A - Toy Cars.cpp
@@ -1,3 +1,9 @@
+/*
+	Solution Explanation:
+		A car has turned over if and only if it's row contains a 1 or 3. Thus, if a row contains no 1s or 3s, that
+		row corresponds to a car that has not turned over. Add such cars to an array or vector, and print that
+		array / vector.
+*/
 #include <iostream>
 #include <vector>
 using namespace std;
