ac729dd255814adcdbd150adb5dc06984a589611
ynassar <yousefnassar@aucegypt.edu>
2016-04-10 18:38:32 +0200

    Added Explanation

--- Codeforces-Solutions/58A - Chat Room.cpp
+++ Codeforces-Solutions/58A - Chat Room.cpp
@@ -1,3 +1,10 @@
+/*
+	To find out whether a word can be made into "hello" or not, we need to check if there are the letters "h", "e", "l", "l", and
+	"o" in that order contained in the word. To check this, we loop over the string with an integer representing the number of
+	the letter in the string "hello" that is supposed to come next. If we find this letter, increment the integer. If the integer
+	reaches the length of the word "hello", that means that the letters of "hello" were found in that order, and we can make the
+	word into "hello".
+*/
 #include <iostream>
 #include <string>
 using namespace std;
