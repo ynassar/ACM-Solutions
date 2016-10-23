ff1a061f62342f2a88f309041cfcc945107035f8
Yousef Nassar <yousefnassar@aucegypt.edu>
2016-10-23 19:43:01 +0200

    Create IOIPALIN.cpp

---
+++ SPOJ-Solutions/IOIPALIN.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<algorithm>
+
+using namespace std;
+
+int n;
+string s;
+int dp[5007][5007];
+
+int palin(int i, int j)
+{
+	
+	if (i >= j)
+		return 0;
+
+	int &r = dp[i][j];
+	
+	if (r != -1)
+		return r;
+
+	if (s[i] == s[j])
+		return r = palin(i + 1, j - 1);
+
+	return r = min(1 + palin(i, j - 1), 1 + palin(i + 1, j));
+
+}
+
+int main()
+{
+	
+
+	cin >> n >> s;
+	memset(dp, -1, sizeof dp);
+	cout << palin(0, n - 1) << endl;
+}
