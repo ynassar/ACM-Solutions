f31e1c0b5715b772f138b6f45839f4605640bbd5
Yousef Nassar <yousefnassar@aucegypt.edu>
2016-11-20 13:11:20 +0200

    Create 101086H - Another Square in the Floor.cpp

---
+++ Codeforces-Solutions/101086H - Another Square in the Floor.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int x, y; cin >> x >> y;
+        cout << max(x,y) * max(x,y) << endl;
+    }
+}
